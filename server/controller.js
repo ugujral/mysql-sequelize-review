b953c1038fb8eb33e7323a27eaad1b6b68352712
Yongsoo Lee <yongsoo07@gmail.com>
2019-03-14 11:50:20 -0700

    init

---
+++ server/controller.js
@@ -0,0 +1,20 @@
+const controller = {
+  get: (req, res) => {
+    // todo
+    res.send('hello from get');
+  },
+  post: (req, res) => {
+    // todo
+    res.send('hello from post');
+  },
+  update: (req, res) => {
+    // todo
+    res.send('hello from update');
+  },
+  delete: (req, res) => {
+    // todo
+    res.send('hello from delete');
+  },
+};
+
+module.exports = controller;
